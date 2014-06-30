6c601903103934680dce72af7ad5cb101bf543c8
Martin Fenner <mfenner@plos.org>
2014-06-30 13:37:11 +0200

    added additional ALM providers

--- alm-oembed.php
+++ alm-oembed.php
@@ -1,17 +1,17 @@
 <?php
 
-/* 
+/*
 	Plugin Name: ALM oEmbed
 	Plugin URI: https://github.com/articlemetrics/alm-oembed-plugin
 	Description: Enable oEmbed for Article-Level Metrics applications
 	Author: Martin Fenner
-	Version: 1.0.0
-	Author URI: http://blogs.plos.org/mfenner
-	
-  Copyright 2013 Public Library of Science
+	Version: 1.0.3
+	Author URI: http://blog.martinfenner.org
+
+  Copyright 2013-2014 Public Library of Science
 
   This program is free software; you can redistribute it and/or modify
-  it under the terms of the GNU General Public License, version 2, as 
+  it under the terms of the GNU General Public License, version 2, as
   published by the Free Software Foundation.
 
   This program is distributed in the hope that it will be useful,
@@ -22,15 +22,17 @@
   You should have received a copy of the GNU General Public License
   along with this program; if not, write to the Free Software
   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-	
+
 	Usage:
   This plugin enables embedding of Article-Level Metrics.
 
 */
 
-wp_oembed_add_provider('http://alm.plos.org/articles/*','http://alm.plos.org/oembed',true);
-wp_oembed_add_provider('http://alm2-iad.plos.org/articles/*','http://alm.plos.org/oembed',true);
-wp_oembed_add_provider('http://alm.publicknowledgeproject.org/articles/*','http://alm.publicknowledgeproject.org/oembed',true);
-wp_oembed_add_provider('http://alm.copernicus.org/articles/*','http://alm.copernicus.org/oembed',true);
+wp_oembed_add_provider('http://alm.plos.org/articles/*','http://alm.plos.org/oembed');
+wp_oembed_add_provider('http://pkp-alm.lib.sfu.ca/articles/*','http://pkp-alm.lib.sfu.ca/oembed');
+wp_oembed_add_provider('http://metricus.copernicus.org/articles/*','http://metricus.copernicus.org/oembed');
+wp_oembed_add_provider('http://labs.crowdometer.org/articles/*','http://labs.crowdometer.org/oembed');
+wp_oembed_add_provider('http://alm.labs.crossref.org/articles/*','http://alm.labs.crossref.org/oembed');
+wp_oembed_add_provider('http://alm.svr.elifesciences.org/articles/*','http://alm.svr.elifesciences.org/oembed');
 
 ?>
--- readme.txt
+++ readme.txt
@@ -2,16 +2,16 @@
 Contributors: mfenner
 Tags: oembed, embed, alm, altmetrics, metrics, doi, plos, res-comms
 Requires at least: 3.2
-Tested up to: 3.5.1
-Stable tag: 1.0
+Tested up to: 3.9.1
+Stable tag: 1.0.3
 License: GPLv2
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
-Paste in a ALM article URL to a page or post, and it will display the citation with metrics. 
+Paste in a ALM article URL to a page or post, and it will display the citation with metrics.
 
 == Description ==
 
-Paste in a ALM article URL to a page or post, and it will display the citation with metrics. 
+Paste in a ALM article URL to a page or post, and it will display the citation with metrics.
 
 == Installation ==
 
@@ -20,6 +20,18 @@ Paste in a ALM article URL to a page or post, and it will display the citation w
 3. Paste a PLOS article URL into a page or post.
 
 == Changelog ==
+
+= 1.0.3 =
+
+Added additional ALM servers.
+
+= 1.0.2 =
+
+Use DOI as oEmbed endpoints
+
+= 1.0.1 =
+
+Updated oEmbed endpoints
 
 = 1.0 =
 
